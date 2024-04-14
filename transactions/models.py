50a5a4d3beb74afa9129f8f8a48e9e3608f12bd2
deep-commitx <i.am.deepak@outlook.in>
2024-04-15 03:25:12 +0530

    👉 Another Commit Pushed Successfully!

--- transactions/models.py
+++ transactions/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 from .constants import TRANSACTION_TYPE_CHOICES
 from accounts.models import UserBankAccount
