25a56bcb5fe32a0e373ac79aad99cb1d48f7e41d
deep-commitx <i.am.deepak@outlook.in>
2024-04-15 01:15:25 +0530

    👉 Another Commit Pushed Successfully!

--- AIBS/urls.py
+++ AIBS/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from core.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path(
+        'transactions/',
+        include('transactions.urls', namespace='transactions')
+    )
 ]
